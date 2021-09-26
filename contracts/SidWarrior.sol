d23a4215164940bb604996d55e453e7623ebd842
siddharth <siddharthbhoite22@gmail.com>
2021-09-26 15:22:04 +0530

    Second commmit

--- contracts/SidWarrior.sol
+++ contracts/SidWarrior.sol
@@ -6,19 +6,23 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract SidWarrior is ERC721, Ownable { 
-    
+
     uint256 public tokenCounter = 0;
 
     constructor() public ERC721("SidWarrior", "SID") {
         tokenCounter = 0;
     }
 
-    function createCollectible(string[] memory tokenURIArr) public  {
-        for (uint256 i = 0; i < tokenURIArr.length; i++) {
+    function createCollectible(string[] memory tokenURIArr) public  { 
+
+        for (uint256 i = 0; i < tokenURIArr.length; i++) { 
+
             uint256 newItemId = tokenCounter;
             _safeMint(msg.sender, newItemId);
             _setTokenURI(newItemId, tokenURIArr[i]);
-            tokenCounter += 1;
-        }
+            tokenCounter += 1; 
+            
+        } 
+
     }
 }
